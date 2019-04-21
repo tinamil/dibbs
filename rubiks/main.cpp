0e0a53edcba4535c4ecda25a439cbe1a47146eb1
John Pavlik <john.a.pavlik@gmail.com>
2019-04-21 00:43:00 -0500

    Add defines to control searches

--- rubiks/main.cpp
+++ rubiks/main.cpp
@@ -8,27 +8,36 @@
 using namespace std;
 using namespace Rubiks;
 
+#define ASTAR 
+#define DIBBS 
+
 void search_cubes()
 {
   vector<uint8_t*> cubes = RubiksLoader::load_cubes("korf1997.txt");
   PDB type = PDB::a12;
   vector<uint64_t> count_results;
-  vector<long> time_results;
+  vector<int64_t> time_results;
 
+  clock_t c_start, c_end;
+  int64_t time_elapsed_ms;
   for (size_t i = 0; i < cubes.size(); ++i)
   {
-    clock_t c_start = clock();
-    //count_results.push_back(search::a_star(cubes[i], type));
-    clock_t c_end = clock();
-    auto time_elapsed_ms = (c_end - c_start) / CLOCKS_PER_SEC;
-    //time_results.push_back(time_elapsed_ms);
-    //cout << "IDA* CPU time used: " << time_elapsed_ms << " s" << endl;
+    #ifdef ASTAR
+    c_start = clock();
+    count_results.push_back(search::a_star(cubes[i], type));
+    c_end = clock();
+    time_elapsed_ms = (c_end - c_start) / CLOCKS_PER_SEC;
+    time_results.push_back(time_elapsed_ms);
+    cout << "IDA* CPU time used: " << time_elapsed_ms << " s" << endl;
+    #endif
+    #ifdef DIBBS
     c_start = clock();
     count_results.push_back(search::id_dibbs(cubes[i], type));
     c_end = clock();
     time_elapsed_ms = (c_end - c_start) / CLOCKS_PER_SEC;
     time_results.push_back(time_elapsed_ms);
     cout << "DIBBS CPU time used: " << time_elapsed_ms << " s" << endl;
+    #endif
   }
 
   for (size_t i = 0; i < count_results.size(); ++i) {
